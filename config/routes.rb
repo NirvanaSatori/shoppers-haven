fc24b3e1c6786516c47fd7e1a779c31520d439c5
NirvanaSatori <morris24kariuki@gmail.com>
2023-06-02 07:51:12 +0300

    add fallback controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,22 @@
 Rails.application.routes.draw do
-  resources :feedbacks
-  resources :ratings
-  resources :products
-  resources :ecommerce_websites
-  resources :search_histories 
-  resources :users 
-  # root 'feedbacks#new'
-  # post "contact", to:'feedbacks#new'
-  post "/login", to:"auth#login"
-  delete "/logout", to: "auth#logout"
-  get "/me", to: "users#me"
-  post "/signup", to:"users#signup"
-
+  namespace :api do
+    resources :feedbacks
+    resources :ratings
+    resources :products
+    resources :ecommerce_websites
+    resources :search_histories 
+    resources :users 
+    # root 'feedbacks#new'
+    # post "contact", to:'feedbacks#new'
+    post "/login", to:"auth#login"
+    delete "/logout", to: "auth#logout"
+    get "/me", to: "users#me"
+    post "/signup", to:"users#signup"
+  end
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
