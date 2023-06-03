c99b1218f92714671f4613fb3317cb779d155000
NirvanaSatori <morris24kariuki@gmail.com>
2023-06-04 02:31:02 +0300

    drop the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,9 @@
 
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-EcommerceWebsite.destroy_all
 Product.destroy_all
+EcommerceWebsite.destroy_all
+
 
 
 
