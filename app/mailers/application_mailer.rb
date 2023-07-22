32d723347d9ad92b78af3b5d838347524b6d2d15
NirvanaSatori <morris24kariuki@gmail.com>
2023-07-22 23:56:53 +0300

    change default email in the Application mailer class

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,7 +2,7 @@ require 'action_mailer'
 
 class ApplicationMailer < ActionMailer::Base
 
-  default from: 'dennomaz@gmail.com'
+  default from: 'morris.waithaka42@gmail.com'
 
   layout 'mailer'
 end
